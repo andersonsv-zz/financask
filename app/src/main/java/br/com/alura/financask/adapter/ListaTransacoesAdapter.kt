d9f5ccc1e3cc1c0443d382b4d3269e42732604c0
Your Name <anderson.si.vieira@hotmail.com>
2019-01-28 21:21:16 -0200

    Ajustes, extension

--- app/src/main/java/br/com/alura/financask/adapter/ListaTransacoesAdapter.kt
+++ app/src/main/java/br/com/alura/financask/adapter/ListaTransacoesAdapter.kt
@@ -1,23 +1,46 @@
 package br.com.alura.financask.adapter
 
 import android.content.Context
+import android.support.v4.content.ContextCompat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import br.com.alura.financask.R
+import br.com.alura.financask.extension.formataParaBrasileiro
+import br.com.alura.financask.model.Tipo
+import br.com.alura.financask.model.Transacao
+import kotlinx.android.synthetic.main.transacao_item.view.transacao_categoria
+import kotlinx.android.synthetic.main.transacao_item.view.transacao_data
+import kotlinx.android.synthetic.main.transacao_item.view.transacao_icone
+import kotlinx.android.synthetic.main.transacao_item.view.transacao_valor
 
-
-class ListaTransacoesAdapter(context: Context, transacoes: List<String>) : BaseAdapter(){
+class ListaTransacoesAdapter(context: Context, transacoes: List<Transacao>) : BaseAdapter() {
 
     private val mTransacoes = transacoes
     private val mContext = context
 
     override fun getView(posicao: Int, view: View?, parent: ViewGroup?): View {
-        return LayoutInflater.from(mContext).inflate(R.layout.transacao_item, parent, false)
+        val viewCriada = LayoutInflater.from(mContext).inflate(R.layout.transacao_item, parent, false)
+
+        val transacao = mTransacoes[posicao]
+
+        if (transacao.tipo == Tipo.RECEITA) {
+            viewCriada.transacao_valor.setTextColor(ContextCompat.getColor(mContext, R.color.receita))
+            viewCriada.transacao_icone.setBackgroundResource(R.drawable.icone_transacao_item_receita)
+        } else {
+            viewCriada.transacao_valor.setTextColor(ContextCompat.getColor(mContext, R.color.despesa))
+            viewCriada.transacao_icone.setBackgroundResource(R.drawable.icone_transacao_item_despesa)
+        }
+
+        viewCriada.transacao_valor.text = transacao.valor.toString()
+        viewCriada.transacao_categoria.text = transacao.categoria
+        viewCriada.transacao_data.text = transacao.data.formataParaBrasileiro()
+
+        return viewCriada
     }
 
-    override fun getItem(posicao: Int): String {
+    override fun getItem(posicao: Int): Transacao {
         return mTransacoes[posicao]
     }
 
--- app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/br/com/alura/financask/ui/activity/ListaTransacoesActivity.kt
@@ -2,20 +2,42 @@ package br.com.alura.financask.ui.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.ArrayAdapter
 import br.com.alura.financask.R
 import br.com.alura.financask.adapter.ListaTransacoesAdapter
+import br.com.alura.financask.model.Tipo
+import br.com.alura.financask.model.Transacao
 import kotlinx.android.synthetic.main.activity_lista_transacoes.lista_transacoes_listview
+import java.math.BigDecimal
+import java.util.Calendar
 
-class ListaTransacoesActivity : AppCompatActivity () {
+class ListaTransacoesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_lista_transacoes)
 
-        val transacoes = listOf("Comida - R$ 20,50", "Economia - R$ 100,00")
-        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, transacoes)
+        val transacoes = listOf(
+            Transacao(
+                tipo = Tipo.DESPESA,
+                data = Calendar.getInstance(),
+                valor = BigDecimal(20.5)
+            ),
+            Transacao(
+                valor = BigDecimal(100.0),
+                tipo = Tipo.RECEITA,
+                categoria = "Economia"
+            ),
+            Transacao(
+                valor = BigDecimal(200.0),
+                tipo = Tipo.DESPESA
+            ),
+            Transacao(
+                valor = BigDecimal(500.0),
+                categoria = "Prêmio",
+                tipo = Tipo.RECEITA
+            )
+        )
 
         lista_transacoes_listview.adapter = ListaTransacoesAdapter(this, transacoes)
     }
